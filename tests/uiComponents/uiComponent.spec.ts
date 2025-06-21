2ec6b9e12c4e1460047cb446a64e1a63606f2037
cesar rodriguez <94cesarrh@gmail.com>
2025-06-20 21:46:29 -0600

    section 5, uiComponents - drag & drop with iframes

--- tests/uiComponents/uiComponent.spec.ts
+++ tests/uiComponents/uiComponent.spec.ts
@@ -229,3 +229,19 @@ test('009 - sliders', async ({page}) => {
 
     await expect(tempBox).toContainText('30')
 })
+
+
+test('010 - (drag and drop) & iframes', async ({page}) => {
+    await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
+
+    const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
+    // await frame.locator('li', {hasText: 'High Tatras 2'}).dragTo(frame.locator('#trash'))
+
+    // more precise control
+    await frame.locator('li', {hasText: 'High Tatras 4'}).hover()
+    await page.mouse.down()
+    await frame.locator('#trash').hover()
+    await page.mouse.up()
+
+    await expect(frame.locator('#trash li h5')).toHaveText('High Tatras 4')
+})
