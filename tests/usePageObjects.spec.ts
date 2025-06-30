e2727d5291ebfd66476609a1479eb35aa8982e05
cesar rodriguez <94cesarrh@gmail.com>
2025-06-29 19:43:15 -0600

    parametrized tests demo

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+import { faker } from "@faker-js/faker"
 
 
 test.beforeEach(async ({page}) => {
@@ -18,10 +19,17 @@ test('navigate to form page', async ({page}) => {
 
 test('paramterized methods example', async ({page}) => {
     const pm = new PageManager(page)
-    
+
+    // test data generation:
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const fullName = `${firstName} ${lastName}`
+    const randomNumer = faker.number.int(100)
+    const email = `${firstName}${lastName}${randomNumer}@testmail.com`
+
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test1@gmail.com', 'password1', 'option 1')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameAndEmail('test2@gmail.com', 'Password2', true)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameAndEmail(fullName, email, true)
 
     await pm.navigateTo().datapickerPage()
     await pm.onDatepickerPage().selectCommonDatePickerFromToday(5)
